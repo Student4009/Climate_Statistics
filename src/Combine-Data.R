ae5753b5a6b1c6a8cf4e6818f9e50a52e4d5846a
Student4009 <119950745+Student4009@users.noreply.github.com>
2022-12-06 16:40:07 +0000

    Update Combine-Data.R

--- src/Combine-Data.R
+++ src/Combine-Data.R
@@ -51,3 +51,7 @@ write.table(x = plot_df,
             file = "out/average-rainfall.csv",
             sep = ",",
             row.names = FALSE)
+
+sink(file = "out/package-versions.txt")
+sessionInfo()
+sink()
